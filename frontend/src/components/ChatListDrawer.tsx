5081a21f9419da99e7cd904bc34b2a6c2b8e02e7
wzj5235 <wzj5235@gmail.com>
2024-11-29 10:34:33 -0800

    adjust height 85vh

--- frontend/src/components/ChatListDrawer.tsx
+++ frontend/src/components/ChatListDrawer.tsx
@@ -33,7 +33,7 @@ export function ChatListDrawer({
       open={isDrawerOpen}
       onClose={handleClose}
       className="w-full max-w-md p-0"
-      style={{ height: "90vh" }}
+      style={{ height: "85vh" }}
     >
       <ChatList
         {...{ setCurrCharacter, setIsChatting, setIsDrawerOpen, msg, setMsg, isLogin, setIsLoginModalOpen }}
